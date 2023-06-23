a1284fa46c1f54703887c7af94fc6c15798550d4
Reign <reignzj.westry@gmail.com>
2023-06-22 20:34:27 -0400

    setup updated'

---
+++ KotlinMinutes.kt
@@ -0,0 +1,14 @@
+fun main(){
+    // val firstname = String = "Reign"
+    // var weight = 225 
+    // val myVariable: Boolean = false
+
+    //Type inference = if its obvious what the type is we dont need to explicitly declare it
+
+    // val: read-only
+    // var: read and write
+
+    val s1 = "Call me"
+    val s2 = " maybe"
+    val combined = s1 + s2
+}
